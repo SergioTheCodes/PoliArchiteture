b41c15892694be7779155e2d81951da6e7b8ea0f
Sergio Junco <sergioesteban2049@gmail.com>
2024-09-04 10:17:58 -0500

    Client Service Methods

--- eventhub/src/main/java/com/eventhub/eventhub/models/dto/request/ClientService.java
+++ eventhub/src/main/java/com/eventhub/eventhub/models/dto/request/ClientService.java
@@ -1,11 +1,28 @@
 package com.eventhub.eventhub.models.dto.request;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
 
 import com.eventhub.eventhub.models.dto.response.EventData;
 
 public class ClientService {
+    private String BaseUrl = "https://rest.bandsintown.com";
+    private String appId = "13722599";
     public List<EventData> getEventsByArtistName(String artistName){
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(BaseUrl + "/artists/" + artistName)).build();
+        client.sendAsync(request, BodyHandlers.ofString()).thenApply(HttpResponse::body).thenAccept(System.out::println).join();
+        return null;
+    }
+
+    public List<EventData> getArtistInfoByArtistName(String artistName){
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(BaseUrl + "/artists" + artistName + "/events")).build();
+        client.sendAsync(request, BodyHandlers.ofString()).thenApply(HttpResponse::body).thenAccept(System.out::println).join();
         return null;
     }
 }
